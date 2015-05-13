3c1637ccf603834d629ea2f0ad829b4e4ddf7d03
Scott Dexter <sdexter@brooklyn.cuny.edu>
2015-05-13 15:16:26 -0400

    Grade.

---
+++ FlyingObjectsImproved/Grading.md
@@ -0,0 +1,12 @@
+         | Documentation (15)| Style (25) | Design (20) | Correctness (40) | 
+---------| ---------| ---------| ---------| ---------|---------|
+Excellent|   All javadoc is properly formatted, clear, concise. In-line comments are used judiciously and contribute to reader's understanding of code. "Works Cited" section is complete.           |   Code has a consistent style and is easy to read. Whitespace is used effectively; identifier names contribute to reader's understanding of the code.          | Object-oriented design principles have used correctly and effectively. Inheritance, composition, and other relationships are well-justified, even intuitive.            | Game play works correctly, smoothly, and intuitively   |           |
+Adequate|  Javadocs are present but difficult to read; in-line comments are excessive or underutilized; "Works Cited" section has problems.  | Principles of style are evident but not consistently applied; areas of the code are not easy to read; too many identifiers are unhelpfully named. |  Object-oriented design is present, but some design decisions seem arbitrary or  unjustified             |Game behavior is mostly correct, but has some unexpected/incorrect behavior            |
+Needs Work | Javadoc, in-line comments, and/or "Works Cited" are minimal or missing.            |  No consistent style has been applied; code is difficult to read.           | Little evidence of an object-oriented design   | Code doesn't run; game doesn't really play            |
+
+Documentation (15) =  10. Minimal doccomments and other comments   
+Style (25) =  25. 
+Design (20) =   15.  Objects should be more "object oriented" see comments in SimpleGame  
+Correctness (40) =  35. Why do all the falling objects only fall to the right??   
+  
+Total =   85.  
--- FlyingObjectsImproved/src/SimpleGame.java
+++ FlyingObjectsImproved/src/SimpleGame.java
@@ -20,15 +20,14 @@ public class SimpleGame extends Game {
 	SimpleSpaceObject ship;
 	ControlledObject controlShip;
 	SpaceObject object;
-	SpaceObject [] gameObjects;
+	SpaceObject[] gameObjects;
 
-	//Variables needed for game to keep track of game
+	// Variables needed for game to keep track of game
 	protected static int MAX_OBJECTS = 4;
 	protected static int LEVELS = 5;
 	protected static int SCORE = 0;
 	protected static int MAX_X = 400;
 	protected Random randNum = new Random();
-
 
 	/**
 	 * This constructor invokes the super constructor, then creates a ship
@@ -42,7 +41,7 @@ public class SimpleGame extends Game {
 	public SimpleGame(String name, int inWidth, int inHeight) {
 		super(name, inWidth, inHeight);
 		setBackground(Color.BLACK);
-		addKeyListener(new KeyboardAdapter() );
+		addKeyListener(new KeyboardAdapter());
 		Point[] shipShape = { new Point(210, 100), new Point(190, 90),
 				new Point(200, 100), new Point(190, 110) };
 		ship = new SimpleSpaceObject(shipShape, new Point(200, 300), -90);
@@ -58,30 +57,40 @@ public class SimpleGame extends Game {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.WHITE);
-		g.drawString("SCORE: ",25,25);
-		g.drawString(Integer.toString(SCORE),80,25);
+		g.drawString("SCORE: ", 25, 25);
+		g.drawString(Integer.toString(SCORE), 80, 25);
 		controlShip.paint(g);
+
+		// !! the ship and objects should animate themselves in their paint()
+		// methods; they shoudln't have to be told what to do! at most, what you
+		// should right here is tell everything you want painted to paint
+		// themselves, then ask the ship if it's still 'alive'
+
 		controlShip.move();
-		for(int i = 0 ; i < gameObjects.length ; i++ ){
+		for (int i = 0; i < gameObjects.length; i++) {
 			gameObjects[i].paint(g);
 			gameObjects[i].move(LEVELS, LEVELS);
 			gameObjects[i].rotate(LEVELS);
 			ship.collide(gameObjects[i]);
-			if(ship.getPoly().findArea() == 0) {
+
+			// !! this is particularly hard to read -- why does this mean the
+			// game should be over?
+
+			if (ship.getPoly().findArea() == 0) {
 				g.drawString("Game Over", 150, 200);
-			} 
-			else{
+			} else {
 				SCORE++;
-				g.drawString(Integer.toString(SCORE),80,25);
+				g.drawString(Integer.toString(SCORE), 80, 25);
 			}
 		}
 	}
 
 	public void createObjects(int objSize) {
 		gameObjects = new SpaceObject[objSize];
-		for(int i = 0 ; i < gameObjects.length ; i++){
+		for (int i = 0; i < gameObjects.length; i++) {
 			int randX = randNum.nextInt(MAX_X);
-			Point[] testObj = { new Point(), new Point(), new Point(), new Point(), new Point() };
+			Point[] testObj = { new Point(), new Point(), new Point(),
+					new Point(), new Point() };
 			object = new SimpleSpaceObject(testObj, new Point(randX, 50), -90);
 			gameObjects[i] = new FallingObject(new SpinningObject(object));
 		}
